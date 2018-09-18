1029833b7ee4584ef715c873c4e7fec180688bfb
Radek <radek_prochazka@seznam.cz>
2018-09-18 19:29:53 +0200

    git ignore env files

--- src/server/env/prod.environment.js
+++
@@ -1,8 +0,0 @@
-const cosmosPort = 10255;
-const dbName = 'pointsmanager';
-const key = 'SdVGer0i4P2PFcg5wdl3aIKTRCP3jMRf18ex2cbvdoyLudOzfCWqoSuXco6QyuaNfOoYmjadLipUuGKs1EduNw==';
-const mongoUri = `mongodb://${dbName}:${key}@${dbName}.documents.azure.com:${cosmosPort}/${dbName}?ssl=true`; //&replicaSet=globaldb`;
-
-module.exports = {
-  mongoUri
-};
